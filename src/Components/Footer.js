7454b2d66cc9ded1920b103f67bdd0688ab295c8
Tevin Takata <takatat9@hawaii.edu>
2024-05-26 18:11:55 -1000

    Updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,6 +5,8 @@ const Footer = () => {
   return (
     <Container className="footer">
       "The Daily Puzzle" published by Typosaurus
+
+      This is an unofficial solver created by <a href="https://github.com/tevin-takata">Tevin Takata</a>
     </Container>
   );
 }
