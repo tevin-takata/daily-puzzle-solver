c4a5f02218b25558ea996b9e178eef2670aace64
Tevin Takata <takatat9@hawaii.edu>
2024-05-26 21:35:42 -1000

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,9 +4,8 @@ import { Container } from 'react-bootstrap';
 const Footer = () => {
   return (
     <Container className="footer">
-      "The Daily Puzzle" published by Typosaurus
-
-      This is an unofficial solver created by <a href="https://github.com/tevin-takata">Tevin Takata</a>
+      <p>"The Daily Puzzle" published by Typosaurus</p>
+      <p>This is an unofficial solver created by <a href="https://github.com/tevin-takata">Tevin Takata</a></p>
     </Container>
   );
 }
