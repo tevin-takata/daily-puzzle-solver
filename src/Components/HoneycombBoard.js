a1748063a3f6ffe58a68d7546fb285dd803f5fa9
Tevin Takata <takatat9@hawaii.edu>
2024-05-29 20:04:22 -1000

    Honeycomb has function in console

--- src/Components/HoneycombBoard.js
+++ src/Components/HoneycombBoard.js
@@ -2,15 +2,8 @@ import {useState, useEffect} from 'react';
 import { Button, Container } from 'react-bootstrap';
 import words from '../words';
 
-let defaultBoard = [7];
-for (let i = 0; i < 7; i++) {
-  defaultBoard[i] = '';
-}
-
-let solvedWords = [5];
-for (let i = 0; i < 5; i++) {
-  solvedWords[i] = [];
-}
+const defaultBoard = new Array(7).fill('');
+const solvedWords = Array.from({ length: 5 }, () => []);
 
 const HoneycombBoard = (props) => {
   const [board, setBoard] = useState(defaultBoard);
@@ -55,38 +48,68 @@ const HoneycombBoard = (props) => {
   });
 
   function solve() {
+    let visited = new Array(7).fill(false);
+    solveHelper("", visited);
+    setSolved(true);
+    console.log(found);
+  }
 
+  function solveHelper(word, visited) {
+    if (words.includes(word.toLowerCase()) && visited[3] === true) {
+      setFound((foundWords) => {
+        if (!foundWords[word.length - 3].includes(word)) {
+          foundWords[word.length - 3].push(word);
+        }
+        return [...foundWords];
+      });
+    }
+
+    for (let i = 0; i < 7; i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        let check = word.concat(board[i]);
+        if (check.length < 3 || !found[check.length - 3]?.includes(check)) {
+          solveHelper(check, visited);
+        }
+        visited[i] = false;
+      }
+    }
   }
 
   return (
-    <div className="layout">
-      <div>
-        <div className="honeycomb">
-          <div className="honeycontent">{board[0]}</div>
+    <div>
+      <div className="layout">
+        <div>
+          <div className="honeycomb">
+            <div className="honeycontent">{board[0]}</div>
+          </div>
+          <div className="honeycomb">
+            <div className="honeycontent">{board[1]}</div>
+          </div>
         </div>
-        <div className="honeycomb">
-          <div className="honeycontent">{board[1]}</div>
+        <div>
+          <div className="honeycomb">
+            <div className="honeycontent">{board[2]}</div>
+          </div>
+          <div className="center-honeycomb">
+            <div className="honeycontent">{board[3]}</div>
+          </div>
+          <div className="honeycomb">
+            <div className="honeycontent">{board[4]}</div>
+          </div>
+        </div>
+        <div>
+          <div className="honeycomb">
+            <div className="honeycontent">{board[5]}</div>
+          </div>
+          <div className="honeycomb">
+            <div className="honeycontent">{board[6]}</div>
+          </div>
         </div>
       </div>
-      <div>
-        <div className="honeycomb">
-          <div className="honeycontent">{board[2]}</div>
-        </div>
-        <div className="center-honeycomb">
-          <div className="honeycontent">{board[3]}</div>
-        </div>
-        <div className="honeycomb">
-          <div className="honeycontent">{board[4]}</div>
-        </div>
-      </div>
-      <div>
-        <div className="honeycomb">
-          <div className="honeycontent">{board[5]}</div>
-        </div>
-        <div className="honeycomb">
-          <div className="honeycontent">{board[6]}</div>
-        </div>
-      </div>
+      <Button className="button" onClick={() => setClick(true)}>
+        SOLVE
+      </Button>
     </div>
   );
 }
